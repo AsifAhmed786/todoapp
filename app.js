42ca567e3e2a97dc28f3c9eca49ee63431fa2473
asifahmed786 <asifahmed1us1@gmail.com>
2020-07-21 13:06:31 +0500

    final todoapp

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ function addTask() {
         var td3 = document.createElement("td")
         td3.setAttribute("class", "text-center")
         var textNode = document.createTextNode(task)
-        td1.appendChild(textNode)
+        //td1.appendChild(textNode)
+        td1.innerHTML = task +"<p class='date1'>Created on: "+new Date().getDate()+"/"+new Date().getMonth()+"/"+new Date().getFullYear()+"</p>";    
         td2.innerHTML = "<i class='fa fa-pencil' onclick='editTask(this)' aria-hidden='true'></i>"
         td3.innerHTML = "<i class='fa fa-trash' onclick='deleteTask(this)' aria-hidden='true'></i>"
         tr.appendChild(td1)
@@ -35,12 +36,22 @@ function changeTheme(img){
 
 
 function deleteTask(e){
-    alert(e)
+    // alert(e.parentNode)
+    // document.remove(e.parentNode)
+    e.parentNode.parentNode.remove()
+    
 
 }
 
 function editTask(e){
-    // alert(task1.par)
+    var existingTask = (e.parentNode.parentNode.childNodes[0].innerHTML).substring(0,(e.parentNode.parentNode.childNodes[0].innerHTML).indexOf("<"))
+    var editTask = prompt("Please update task",existingTask);
+    if (editTask == null || editTask == "") {
+        alert("Please enter something")
+    }
+    else {  
+        e.parentNode.parentNode.childNodes[0].innerHTML = editTask+"<p class='date1'>Edited on: "+new Date().getDate()+"/"+new Date().getMonth()+"/"+new Date().getFullYear()+"</p>";
+    }
     
        
 }
